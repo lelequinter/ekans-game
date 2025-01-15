0cbfdf75b4b821c2654f1e175ec728cd3ce952ed
Leyder Quintero <desarrollador9@wedevel.co>
2025-01-14 19:14:40 -0500

    Screen Setup and Creating a Snake Body

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-# This is a sample Python script.
+from turtle import Screen, Turtle
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor('black')
+screen.title('Ekans Game')
+
+snake_body = []
+
+# Creating snake body
+for index in range(3):
+    new_segment = Turtle('square')
+    new_segment.color('white')
+    x_position = (20 * index) * (-1)
+    new_segment.goto(x_position, 0)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+screen.exitonclick()
